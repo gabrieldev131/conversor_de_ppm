4d6c6add7dbea21aac773f03e74283f26c2ad0e2
gabrieldev131 <119118315+gabrieldev131@users.noreply.github.com>
2023-10-23 13:01:07 -0300

    image.c

--- image.c
+++ image.c
@@ -3,12 +3,20 @@
 #include <string.h>
 #include "image.h"
 
+struct imagem{
+  char tipo[3]; //tipo do cabeçalho da imagem
+  int rows;     //dimensões das linhas
+  int cols;     //dimensões das colunas
+  int max;      //valor máximo do pixel
+  int **pixels; //matriz de pixels
+};
+
 //função que automatiza a busca das informações para create
 Image *getInfo(char *filename){
   FILE *file = fopen(filename, "r");
   if (file == NULL) {
       printf("Erro: não foi possível abrir o arquivo.\n");
-      exit(EXIT_FAILURE);
+      exit(EXIT_FAILURE); //exit_failure é uma macro definida em stdlib.h
   }
   char tipo[3];
   int rows, cols, max, **pixels;
@@ -37,7 +45,7 @@ Image *create(int rows, int cols, char type[]){
     newImage->rows = rows;
     newImage->cols = cols;
     newImage->max = 255;          
-    strncpy(newImage->tipo, type, sizeof(newImage->tipo));
+    strncpy(newImage->tipo, type, sizeof(newImage->tipo)); //função que copia a string e a cola no local indicado
     if (strcmp(type, "P2") == 0) {
         // Aloca memória para matriz de pixels do tipo P2
         newImage->pixels = (int**)malloc(rows * sizeof(int*));
@@ -144,8 +152,8 @@ void rgb_to_gray(Image *image_rgb, Image *image_gray){
           float r = image_rgb->pixels[i][j];
           float g = image_rgb->pixels[i][j + 1];
           float b = image_rgb->pixels[i][j + 2];
-          float gray_value = 0.299 * r + 0.587 * g + 0.114 * b;
-          image_gray->pixels[i][j / 3] = gray_value;
+          float gray_value = 0.299 * r + 0.587 * g + 0.114 * b; //Usadno float por conta da multiplicação
+          image_gray->pixels[i][j / 3] = gray_value; //O valor float é convertido em int automáticamente
         }
     }
 }
