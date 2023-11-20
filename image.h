878532af80ea5134b3e2364a923e9372c05ecd28
gabrieldev131 <119118315+gabrieldev131@users.noreply.github.com>
2023-11-20 16:05:40 -0300

    image.h

--- image.h
+++ image.h
@@ -3,21 +3,13 @@
 
 typedef struct imagem Image;
 
-//função que busca as informações da imagem
+int rows(Image *ima);
+int cols(Image *ima);
+char *tipo(Image *ima);
 Image *getInfo(char *filename);
-
-//função que libera imagem
 void liberarImagem(Image *imagem);
-
-//Aloca memória para a estrutura nas proporções dadas no arquivo de entrada
 Image *create(int rows, int cols, char type[]);
-
-//Carrega a imagem a ser convertida
 Image *load_from_ppm(const char *filename);
-
-//cria/altera uma arquivo do qual será escrito o arquivo convertido
 void write_to_ppm(Image *image, const char *filename);
-
-//função que converte uma foto colorida em lvl de cinza
 void rgb_to_gray(Image *image_rgb, Image *image_gray);
 #endif
